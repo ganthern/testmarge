fedb1c8ca58d5f6ddcd8509ccb19493cd27557f3
ganthern <nils.ganther@stud.hs-hannover.de>
2023-12-01 22:19:16 +0100

    loop

--- prep.bash
+++ prep.bash
@@ -1,4 +1,25 @@
 #! /bin/bash
 
-TST=$(date +%s)
-git switch -c $TST
+function gen {
+  TST=$(date +%s)
+  git switch -c $TST
+
+  echo $TST > file.txt
+
+  git add .
+  git commit -m $TST
+  git push -u origin $TST
+
+  sleep .5
+}
+
+git reset --hard
+git switch main
+
+for i in $(seq 1 10);
+do
+    gen;
+done
+
+
+git switch main
